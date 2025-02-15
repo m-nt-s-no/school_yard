17c412cc105ab2e68ba51387e9b7889afad10c33
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2025-02-15 22:21:53 +0000

    editing calendar start and end times

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
 
   def calendar
     start_date = Date.today.to_date
-    @my_calendar = @user.calendar.where(starts_at: start_date.beginning_of_month.beginning_of_week..start_date.end_of_month.end_of_week)
+    @my_calendar = @user.calendar.where(starts_at: start_date.beginning_of_month..start_date.end_of_month)
   end
 
   private
