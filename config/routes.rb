c3baa199ad8972078f3150f7d0b213bc75a3c7a1
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2025-02-17 03:25:36 +0000

    added as: pathnames to user routes; edited user events view

--- config/routes.rb
+++ config/routes.rb
@@ -8,10 +8,10 @@ Rails.application.routes.draw do
   resources :events
   resources :messages
 
-  get "/directory" => "users#index"#, as: :directory
-  get ":name/events" => "users#events"#, as: :my_events
-  get ":name/groups" => "users#groups"#, as: :my_groups
-  get ":name/messages" => "users#messages"#, as: :my_messages
+  get "/directory" => "users#index", as: :directory
+  get ":name/events" => "users#events", as: :my_events
+  get ":name/groups" => "users#groups", as: :my_groups
+  get ":name/messages" => "users#messages", as: :my_messages
   get ":name/calendar" => "users#calendar", as: :my_calendar
   get ":name" => "users#show", as: :user
 end
